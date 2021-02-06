8f07f67b3afc595b07ccc9fd6024d57eea675ed3
amiruddin1 <67670878+amiruddin1@users.noreply.github.com>
2021-02-06 08:33:59 +0530

    Update FileExtension.py

--- FileExtension.py
+++ FileExtension.py
@@ -2,4 +2,4 @@ fn=input("Enter File Name: ")
 #Enter File Name: abc.txt
 >>> f=fn.split(".")
 >>> print ("Extansion of the file is: " + f[-1])
-Extansion of the file is: txt
+#Extansion of the file is: txt
